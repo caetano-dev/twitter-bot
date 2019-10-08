feec6380dd9f74f584e78fba24c3de929fca1c3c
ImGabe <gabrielpmonte@hotmail.com>
2019-10-08 13:54:05 -0300

    catch IndexError

--- main.py
+++ main.py
@@ -57,3 +57,5 @@ if __name__ == '__main__':
         main()
     except KeyboardInterrupt:
         pass
+    except IndexError:
+        print("Missing arguments (follow/unfollow).\nRead the README for more information.")
